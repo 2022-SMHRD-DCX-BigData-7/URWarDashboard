e2831784ad3005ddb93d382723a799d49936f7fb
smart <smart@smhrd>
2023-07-25 14:59:12 +0900

    뉴스 탭 통계부분 제작

---
+++ gittest/src/main/webapp/js/sejin.js
@@ -0,0 +1,49 @@
+// 뉴스기사
+
+function resizeIframe() {
+    var iframe = document.getElementById('myIframe');
+    iframe.style.height = "100%"; // 부모 요소의 높이에 맞게 iframe 높이 조정
+}
+
+// 페이지 크기 변화를 감지하여 resizeIframe 함수 호출
+window.addEventListener('resize', resizeIframe);
+
+
+
+    // 월별 뉴스기사 수 나타내는 기능
+var iframeContainer = document.getElementById('iframeContainer');
+var button = document.getElementById('newsButton');
+var isIframeVisible = false; // 기본적으로 iframe을 표시합니다.
+
+function toggleIframe() {
+    if (isIframeVisible) {
+        iframeContainer.style.display = "none";
+         // iframe을 숨깁니다.    
+    } else {
+        iframeContainer.style.display = "block";
+        iframeContainer2.style.display = "none"; // iframe을 표시합니다.
+    }
+    isIframeVisible = !isIframeVisible;
+}
+// 버튼 클릭 시 toggleIframe 함수 호출
+button.addEventListener('click', toggleIframe);
+
+
+    // 키워드 건수 나타내는 기능
+var iframeContainer2 = document.getElementById('iframeContainer2');
+var keybutton = document.getElementById('keyButton');
+var isIframeVisible2 = false; // 기본적으로 iframe을 표시합니다.
+
+function toggleIframe2() {
+    if (isIframeVisible2) {
+        iframeContainer2.style.display = "none";
+         // iframe을 숨깁니다.    
+    } else {
+        iframeContainer2.style.display = "block";
+        iframeContainer.style.display = "none"; // iframe을 표시합니다.
+    }
+    isIframeVisible2 = !isIframeVisible2;
+}
+// 버튼 클릭 시 toggleIframe 함수 호출
+keybutton.addEventListener('click', toggleIframe2);
+    
